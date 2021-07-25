6b42ad6a173152ea7b71fdc06edd9dc33758035c
Dale.Yarborough <dale.yarborough@wholefoods.com>
2021-07-25 17:20:46 -0500

    Adding basic TensorFlow demo

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.python.ops.gen_math_ops import DenseBincount
+
+
+# model = keras.Sequential(
+#     layers.Dense(2, activation="relu". name="layer1"),
+#     layers.Dense(3, activation="relu", name="layer2"),
+#     layers.Dense(4, name="layer3")
+# )
